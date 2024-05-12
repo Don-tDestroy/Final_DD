80e7f950cdc6b66188cac862fa6dc542da3a111c
seohyun-lee <applemint2003@gmail.com>
2024-05-13 01:35:52 +0900

    feat: AR 카메라 촬영 및 전환 기능 구현
    - TakePhotos.cs: 카메라 촬영 스크립트. 저장 경로를 PC와 Android 경로를 다르게 설정함.
    - CameraSwitch.cs: 카메라 전환 스크립트. 모바일에서만 구동, user<->world
    - Android Logcat 설치함
    - 빌드용 keystore 업로드

--- Assets/Scripts/CameraScripts/TakePhotos.cs
+++ Assets/Scripts/CameraScripts/TakePhotos.cs
@@ -5,48 +5,67 @@ using UnityEngine;
 
 public class TakePhotos : MonoBehaviour
 {
+    // 사진 촬영 메서드
     public void TakePhoto()
     {
+        if (Camera.main == null)
+        {
+            Debug.LogError("카메라를 찾을 수 없습니다 - TakePhoto()");
+        }
         StartCoroutine(TakeAPhoto());
     }
 
     IEnumerator TakeAPhoto()
     {
         yield return new WaitForEndOfFrame();
-        // 카메라
+
         Camera camera = Camera.main;
-        
+
         // 카메라에 Render Texture 설정
         int width = Screen.width;
         int height = Screen.height;
         RenderTexture rt = new RenderTexture(width, height, 24);
         camera.targetTexture = rt;
 
-        // The Render Texture in RenderTexture.active is the one
-        // that will be read by ReadPixels.
+        // RenderTexture.active에 있는 Render Texture가 ReadPixels로 읽힐 Render Texture임
         var currentRT = RenderTexture.active;
         RenderTexture.active = rt;
 
-        // Render the camera's view.
+        // 카메라의 뷰를 렌더링함
         camera.Render();
 
-        // Make a new texture and read the active Render Texture into it.
+        // 새 텍스처를 생성하고 활성 Render Texture를 읽어들임
         Texture2D image = new Texture2D(width, height);
         image.ReadPixels(new Rect(0, 0, width, height), 0, 0);
         image.Apply();
 
         camera.targetTexture = null;
 
-        // Replace the original active Render Texture.
+        // 원래의 활성 Render Texture를 대체함
         RenderTexture.active = currentRT;
 
-        byte[] bytes = image.EncodeToPNG();
+        // 사진 저장 관련 정보
         string fileName = DateTime.Now.ToString("yyyyMMdd_HHmmss") + ".png";
-        string filePath = Path.Combine(Application.persistentDataPath, fileName);
-        File.WriteAllBytes(filePath, bytes);
+        string SavePath = Application.persistentDataPath;
+        string dirName = "Exploring Ewha With Diddy";
+
+        string galleryPath;
+        if (SavePath.IndexOf("Android") > 0) // 안드로이드 플랫폼인 경우 DCIM 하위에 폴더를 생성
+            galleryPath = Path.Combine(SavePath.Substring(0, SavePath.IndexOf("Android")) + "/DCIM/", dirName);
+        else // 경로가 없을 때(PC 플레이 모드 등) 에러 발생 방지
+            galleryPath = Path.Combine(Application.persistentDataPath, dirName);
+
+        if (!Directory.Exists(dirName)) // 해당 디렉토리 없을 시 생성
+        {
+            Directory.CreateDirectory(galleryPath);
+            Debug.Log("폴더를 생성하였습니다. 폴더 경로: " + galleryPath);
+        }
+        string filePath = Path.Combine(galleryPath, fileName);
+
+        File.WriteAllBytes(filePath, image.EncodeToPNG());
+        Debug.Log("사진이 촬영되었습니다. 파일 경로: " + filePath);
 
         Destroy(rt);
         Destroy(image);
     }
-
 }
