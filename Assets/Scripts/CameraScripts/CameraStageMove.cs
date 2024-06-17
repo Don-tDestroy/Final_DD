178dd9d67dccb22c8f4715b293534d7cb0d7ee2a
suabae <bsa0322@ewhain.net>
2024-06-17 14:12:12 +0900

    feat: scene 0 game quit popup

--- Assets/Scripts/CameraScripts/CameraStageMove.cs
+++ Assets/Scripts/CameraScripts/CameraStageMove.cs
@@ -12,6 +12,22 @@ public class CameraStageMove : MonoBehaviour
 
     public void goToNextStage()
     {
-        SceneManager.LoadScene(currStage + 1);
+        switch (currStage)
+        {
+            case 2: // stage 2 -> 3
+                SceneManager.LoadScene("Scene_3");
+                break;
+            case 3: // stage 3 -> 4
+                SceneManager.LoadScene("Scene_4_Before");
+                break;
+            case 4: // stage 4 -> 5
+                SceneManager.LoadScene("Scene_5_Before");
+                break;
+            case 5: // stage 5 -> 6
+                SceneManager.LoadScene("Scene_6_GPS");
+                break;
+            default:
+                break;
+        }
     }
 }
